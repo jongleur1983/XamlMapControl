257b0a6528068428e481ec5f5e7323e4345e50ff
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 08:12:33 +0200

    use more DP by generics

--- MapControl/MapBase.WPF.cs
+++ MapControl/MapBase.WPF.cs
@@ -16,69 +16,99 @@ namespace MapControl
         public static readonly DependencyProperty ForegroundProperty =
             System.Windows.Controls.Control.ForegroundProperty.AddOwner(typeof(MapBase));
 
-        //public static readonly DependencyProperty CenterProperty = GenericDependencyProperty.Register<Location, MapBase>(
-        //    mb => mb.Center,
-        //    new GenericFrameworkPropertyMetadata<Location, MapBase>(
-        //        new Location(),
-        //        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-        //        (mb, args) => mb.CenterPropertyChanged(args.NewValue)));
-        // TODO: replace, requires https://github.com/jongleur1983/genericDependencyProperties/issues/15
-        public static readonly DependencyProperty CenterProperty = DependencyProperty.Register(
-            nameof(Center),
-            typeof(Location),
-            typeof(MapBase),
-            new FrameworkPropertyMetadata(
+        public static readonly DependencyProperty CenterProperty = GenericDependencyProperty.Register<Location, MapBase>(
+            mb => mb.Center,
+            new GenericFrameworkPropertyMetadata<Location, MapBase>(
                 new Location(),
                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                (o, e) => ((MapBase)o).CenterPropertyChanged((Location)e.NewValue)));
+                (mb, args) => mb.CenterPropertyChanged(args.NewValue)));
+        // replaced by the generic variant above:
+        //public static readonly DependencyProperty CenterProperty = DependencyProperty.Register(
+        //    nameof(Center),
+        //    typeof(Location),
+        //    typeof(MapBase),
+        //    new FrameworkPropertyMetadata(
+        //        new Location(),
+        //        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //        (o, e) => ((MapBase)o).CenterPropertyChanged((Location)e.NewValue)));
 
-        // TODO: replace, requires https://github.com/jongleur1983/genericDependencyProperties/issues/15
-        public static readonly DependencyProperty TargetCenterProperty = DependencyProperty.Register(
-            nameof(TargetCenter),
-            typeof(Location),
-            typeof(MapBase),
-            new FrameworkPropertyMetadata(
-                new Location(), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                (o, e) => ((MapBase)o).TargetCenterPropertyChanged((Location)e.NewValue)));
-
-        // TODO: replace, requires https://github.com/jongleur1983/genericDependencyProperties/issues/15
-        public static readonly DependencyProperty ZoomLevelProperty = DependencyProperty.Register(
-            nameof(ZoomLevel),
-            typeof(double),
-            typeof(MapBase),
-            new FrameworkPropertyMetadata(
-                1d, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                (o, e) => ((MapBase)o).ZoomLevelPropertyChanged((double)e.NewValue)));
-
-        // TODO: replace, requires https://github.com/jongleur1983/genericDependencyProperties/issues/15
-        public static readonly DependencyProperty TargetZoomLevelProperty = DependencyProperty.Register(
-            nameof(TargetZoomLevel),
-            typeof(double),
-            typeof(MapBase),
-            new FrameworkPropertyMetadata(
-                1d,
+        public static readonly DependencyProperty TargetCenterProperty = GenericDependencyProperty.Register(
+            mb => mb.TargetCenter,
+            new GenericFrameworkPropertyMetadata<Location, MapBase>(
+                new Location(),
                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                (o, e) => ((MapBase)o).TargetZoomLevelPropertyChanged((double)e.NewValue)));
+                (mb, args) => mb.TargetCenterPropertyChanged(args.NewValue)));
+        // replaced by the generic variant above:
+        //public static readonly DependencyProperty TargetCenterProperty = DependencyProperty.Register(
+        //    nameof(TargetCenter),
+        //    typeof(Location),
+        //    typeof(MapBase),
+        //    new FrameworkPropertyMetadata(
+        //        new Location(), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //        (o, e) => ((MapBase)o).TargetCenterPropertyChanged((Location)e.NewValue)));
 
-        // TODO: replace, requires https://github.com/jongleur1983/genericDependencyProperties/issues/15
-        public static readonly DependencyProperty HeadingProperty = DependencyProperty.Register(
-            nameof(Heading),
-            typeof(double),
-            typeof(MapBase),
-            new FrameworkPropertyMetadata(
-                0d,
+        public static readonly DependencyProperty ZoomLevelProperty = GenericDependencyProperty.Register(
+            mb => mb.ZoomLevel,
+            new GenericFrameworkPropertyMetadata<double, MapBase>(
+                1,
                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                (o, e) => ((MapBase)o).HeadingPropertyChanged((double)e.NewValue)));
+                (mb, args) => mb.ZoomLevelPropertyChanged(args.NewValue)));
+        // replaced by the generic variant above:
+        //public static readonly DependencyProperty ZoomLevelProperty = DependencyProperty.Register(
+        //    nameof(ZoomLevel),
+        //    typeof(double),
+        //    typeof(MapBase),
+        //    new FrameworkPropertyMetadata(
+        //        1d, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //        (o, e) => ((MapBase)o).ZoomLevelPropertyChanged((double)e.NewValue)));
 
-        // TODO: replace, requires https://github.com/jongleur1983/genericDependencyProperties/issues/15
-        public static readonly DependencyProperty TargetHeadingProperty = DependencyProperty.Register(
-            nameof(TargetHeading),
-            typeof(double),
-            typeof(MapBase),
-            new FrameworkPropertyMetadata(
-                0d,
+        public static readonly DependencyProperty TargetZoomLevelProperty = GenericDependencyProperty.Register(
+            mb => mb.TargetZoomLevel,
+            new GenericFrameworkPropertyMetadata<double, MapBase>(
+                1,
                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                (o, e) => ((MapBase)o).TargetHeadingPropertyChanged((double)e.NewValue)));
+                (mb, args) => mb.TargetZoomLevelPropertyChanged(args.NewValue)));
+        // replaced by the generic variant above:
+        //public static readonly DependencyProperty TargetZoomLevelProperty = DependencyProperty.Register(
+        //    nameof(TargetZoomLevel),
+        //    typeof(double),
+        //    typeof(MapBase),
+        //    new FrameworkPropertyMetadata(
+        //        1d,
+        //        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //        (o, e) => ((MapBase)o).TargetZoomLevelPropertyChanged((double)e.NewValue)));
+
+        public static readonly DependencyProperty HeadingProperty = GenericDependencyProperty.Register(
+            mb => mb.Heading,
+            new GenericFrameworkPropertyMetadata<double, MapBase>(
+                0,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                (mb, args) => mb.HeadingPropertyChanged(args.NewValue)));
+        // replaced by the generic variant above:
+        //public static readonly DependencyProperty HeadingProperty = DependencyProperty.Register(
+        //    nameof(Heading),
+        //    typeof(double),
+        //    typeof(MapBase),
+        //    new FrameworkPropertyMetadata(
+        //        0d,
+        //        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //        (o, e) => ((MapBase)o).HeadingPropertyChanged((double)e.NewValue)));
+
+        public static readonly DependencyProperty TargetHeadingProperty = GenericDependencyProperty.Register(
+            mb => mb.TargetHeading,
+            new GenericFrameworkPropertyMetadata<double, MapBase>(
+                0,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                (mb, args) => mb.TargetHeadingPropertyChanged(args.NewValue)));
+        // replaced by the generic variant above:
+        //public static readonly DependencyProperty TargetHeadingProperty = DependencyProperty.Register(
+        //    nameof(TargetHeading),
+        //    typeof(double),
+        //    typeof(MapBase),
+        //    new FrameworkPropertyMetadata(
+        //        0d,
+        //        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //        (o, e) => ((MapBase)o).TargetHeadingPropertyChanged((double)e.NewValue)));
 
         static MapBase()
         {
